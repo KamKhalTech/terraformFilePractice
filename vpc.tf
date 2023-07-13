01f08ab13018ef19dde9d55a6ce4d83e8375c457
DemDevops <126034694+DemDevops@users.noreply.github.com>
2023-07-12 23:52:42 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
-provider "aws" {
+/* provider "aws" {
   region = "us-east-2"
-}
+} */
 
 # Create a VPC
-resource "aws_vpc" "test2" {
+/* resource "aws_vpc" "test2" {
   cidr_block = "10.0.0.0/20"
 
   tags = {
   "Name" = "terravpc"
  }
-}
+} */
