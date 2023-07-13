2f2dca545a32263e71da24b94447edb8c6e208b8
DemDevops <126034694+DemDevops@users.noreply.github.com>
2023-07-12 23:49:51 -0600

    Update function.tf

--- function.tf
+++ function.tf
@@ -3,12 +3,12 @@
 # count = length(var.user_names)
 #name = var.user_names
 #}
-provider "aws" {
+/* provider "aws" {
 
 variable "user_names" {
 type = list(string)
 default = {"Vivian", "George"}
-}
+} */
 
 #output "first_arn" {
 # value = aws_iam_user.example.arn
@@ -25,26 +25,26 @@ default = {"Vivian", "George"}
 # name = each
 #}
 
-resource "aws_iam_user" "for_each_example" {
+/* resource "aws_iam_user" "for_each_example" {
 for_each = tosett(var.users_names)
 name = each
-}
+} */
 
 #moved {
 #from = aws_iam_user.for_each
 #to = aws_iam_user.for_each_example
 #}
 
-output "all_users" {
+/* output "all_users" {
 values = aws_iam_user.for_each_example
 }
 
 output "all_user_arns" {
 value = values(aws_iam_user.for_each_example).arn
-}
+} */
 
 #3. FOR LOOP
-output "upper_names" {
+/* output "upper_names" {
 value =  [ for name in var.user_names : upper(names) if length(name) < 4]
 }
 
@@ -64,7 +64,7 @@ value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
 
 output "upper_roles" {
 value = [ for name, role in var.hero_thousand_faces : upper(name) => upper(role) ]
-}
+} */
 
 
 #{ for name in var.user_names : key => value }
@@ -73,7 +73,7 @@ value = [ for name, role in var.hero_thousand_faces : upper(name) => upper(role)
 #4. Conditional expressions - ternary syntax - returns bool value
 #condition ? true : false
 
-resource "aws_iam_user" "ternary" {
+/* resource "aws_iam_user" "ternary" {
 count = var.check && length(var.user_names) > 0 && length(var.group) > 0 ? length(var.user_names) : 0
 name = var.user_names[count.index]
 }
@@ -86,4 +86,4 @@ default = true
 variable "group" {
 type = list(string)
 default = [ "dev", "prod", "uat"]
-}
+} */
